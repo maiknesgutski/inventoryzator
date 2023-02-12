610fae6f3233995975cbf58b1434f3ea750db76c
maiknesgutski <maik.nesgutski@gmail.com>
2023-02-12 22:30:51 +0100

    tailwind start

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
-export default{
-    fetch(req){
-        return new Response("Hi!");
-    }
-}
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Constants
+const PORT = 9595;
+const HOST = "0.0.0.0";
+
+// APP
+const app = express();
+
+// Host all the static files frome the public directory
+app.use(express.static(path.resolve('public')));
+
+app.listen(PORT, HOST, () => {
+    console.log(`Listening on http://${HOST}:${PORT}`);
+});
